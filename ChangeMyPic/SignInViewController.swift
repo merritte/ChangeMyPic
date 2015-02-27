9be97b799927424419c2dea47dea7ffb4fc309b3
Shay-Jahen Merritte <Shay@Merritte.com>
2015-02-26 20:52:32 -0500

    added twitter api to download profile image

--- ChangeMyPic/SignInViewController.swift
+++ ChangeMyPic/SignInViewController.swift
@@ -26,7 +26,27 @@ class SignInViewController: UIViewController {
         account.requestAccessToAccountsWithType(accountType, options: nil) { (granted:Bool, error: NSError!) -> Void in
             
             if granted {
-                
+                let allAccounts = account.accountsWithAccountType(accountType)
+                if allAccounts.count > 0 {
+                    let twitterAccount = allAccounts.last as ACAccount
+                    
+                    let requestAPI = NSURL(string: "https://api.twitter.com/1.1/account/verify_credentials.json")
+                    
+                    let userRequest = SLRequest(forServiceType: SLServiceTypeTwitter, requestMethod: SLRequestMethod.GET, URL: requestAPI, parameters: nil)
+                    
+                    userRequest.account = twitterAccount
+                    
+                    userRequest.performRequestWithHandler({ (response:NSData!, urlResponse:NSHTTPURLResponse!, error:NSError!) -> Void in
+                        
+                        var error = NSErrorPointer()
+                        
+                        let responseDictionary = NSJSONSerialization.JSONObjectWithData(response, options: NSJSONReadingOptions.MutableLeaves, error: error) as [String : AnyObject]
+                        
+                        let imageURL = responseDictionary["profile_image_url_https"] as NSString
+                        
+                        
+                    })
+                }
             } else {
                 println("Access Not Granted")
             }
